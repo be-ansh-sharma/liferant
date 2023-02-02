68cd97d74d2f5d0c4dd74ccca7f22f419e1b452b
Ansh Sharma <anshsharma60@gmail.com>
2023-02-01 20:57:38 -0500

    fixed the build error

---
+++ src/components/recommendations/Recommendation.js
@@ -0,0 +1,8 @@
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const Recommendations = () => {
+  return <div></div>;
+};
+
+export default Recommendations;
