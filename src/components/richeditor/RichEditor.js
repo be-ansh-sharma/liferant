d9746e361da91f8196faa4ac74a9eaa9fedc9c03
Ansh Sharma <anshsharma60@gmail.com>
2023-03-07 16:22:57 -0500

    added more options in editor

--- src/components/richeditor/RichEditor.js
+++ src/components/richeditor/RichEditor.js
@@ -19,7 +19,6 @@ const RichEditor = ({ fieldInput, fieldInputHandler, maxChar, fieldName }) => {
 
   const checkCharacterCount = event => {
     const unprivilegedEditor = quillRef.current.unprivilegedEditor;
-    console.log(unprivilegedEditor.getText());
     if (unprivilegedEditor.getLength() > maxChar && event.key !== 'Backspace')
       event.preventDefault();
   };
@@ -35,6 +34,27 @@ const RichEditor = ({ fieldInput, fieldInputHandler, maxChar, fieldName }) => {
           ...fieldInput,
           [fieldName]: value,
         });
+      }}
+      modules={{
+        toolbar: [
+          ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+          ['blockquote', 'code-block'],
+
+          [{ header: 1 }, { header: 2 }], // custom button values
+          [{ list: 'ordered' }, { list: 'bullet' }],
+          [{ script: 'sub' }, { script: 'super' }], // superscript/subscript
+          [{ indent: '-1' }, { indent: '+1' }], // outdent/indent
+          [{ direction: 'rtl' }], // text direction
+
+          [{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+          [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+          [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+          [{ font: [] }],
+          [{ align: [] }],
+
+          ['clean'],
+        ],
       }}
     />
   );
